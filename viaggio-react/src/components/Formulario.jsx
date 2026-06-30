4119ce294399580621ebeb55ae351ec24728e630
Morf3uz <vdouglas170@gmail.com>
2026-06-29 22:56:57 -0300

    Commit inicial

--- viaggio-react/src/components/Formulario.jsx
+++
@@ -1,9 +0,0 @@
-function Formulario({ onSubmit, children }) {
-    return (
-        <form onSubmit={onSubmit} noValidate>
-            {children}
-        </form>
-    )
-}
-
-export default Formulario
